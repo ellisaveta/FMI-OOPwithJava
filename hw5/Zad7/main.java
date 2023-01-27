d8aea7b72adfd77cf002554461bbd7680b1f413f
ellisaveta <35685994+ellisaveta@users.noreply.github.com>
2023-01-27 18:26:50 +0200

    Add files via upload

---
+++ hw5/Zad7/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        A a = new A(); //A
+
+        B b = new B(); //A
+                       //B
+
+        C c = new C(); //A
+                       //B
+                       //C
+    }
+}
